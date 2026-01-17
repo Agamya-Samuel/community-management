72a7ccafdbd7443acfe8e68c14f17c79e3d9c24d
Agamya Samuel <github@agamya.dev>
2026-01-17 14:13:39 +0530

    refactor(resizable): simplify component implementation with direct imports
    Replace namespace imports with direct named imports from react-resizable-panels.
    Simplify ResizablePanelGroup to direct Group export and remove unnecessary wrapper.
    Convert ResizableHandle to forwardRef pattern for better ref handling.
    Update ResizablePanel to remove redundant wrapper while maintaining data attributes.

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,41 +2,34 @@
 
 import * as React from 'react'
 import { GripVerticalIcon } from 'lucide-react'
-import * as ResizablePrimitive from 'react-resizable-panels'
+import { Group, Panel, Separator } from 'react-resizable-panels'
 
 import { cn } from '@/lib/utils'
 
-function ResizablePanelGroup({
+const ResizablePanelGroup = Group
+
+function ResizablePanel({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+}: React.ComponentProps<typeof Panel>) {
   return (
-    <ResizablePrimitive.PanelGroup
-      data-slot="resizable-panel-group"
-      className={cn(
-        'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
-        className,
-      )}
+    <Panel
+      data-slot="resizable-panel"
+      className={className}
       {...props}
     />
   )
 }
 
-function ResizablePanel({
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
-  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
-}
-
-function ResizableHandle({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
-  withHandle?: boolean
-}) {
+const ResizableHandle = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentPropsWithoutRef<typeof Separator> & {
+    withHandle?: boolean
+  }
+>(({ withHandle, className, ...props }, ref) => {
   return (
-    <ResizablePrimitive.PanelResizeHandle
+    <Separator
+      elementRef={ref}
       data-slot="resizable-handle"
       className={cn(
         'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
@@ -49,8 +42,9 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </ResizablePrimitive.PanelResizeHandle>
+    </Separator>
   )
-}
+})
+ResizableHandle.displayName = 'ResizableHandle'
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
