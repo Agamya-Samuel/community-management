1bd270ae99ad97bb9c2a73a756220f860e528725
Agamya Samuel <github@agamya.dev>
2026-01-24 01:00:03 +0530

    feat: add new hostname for media uploads in Next.js configuration
    - Include 'upload.wikimedia.org' as an allowed image source for enhanced media handling

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "wikiclubtechshuats.org",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      }
     ],
   },
 };
